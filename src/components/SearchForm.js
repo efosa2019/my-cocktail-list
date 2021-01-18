5d2f42dc6d85b5195aae280c065a64b6319c7790
Efosa Alohan <efosa.alohan@live.com>
2021-01-18 14:51:43 +0100

    Added some styles

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -19,7 +19,7 @@ export default function SearchForm() {
     <section className='section search'>
       <form className='search-form' onSubmit={handleSubmit}>
         <div className='form-control'>
-          <label htmlFor='name'>search your favorite cocktail</label>
+          <label htmlFor='name'>search for your favorite cocktail</label>
           <input
             type='text'
             name='name'
